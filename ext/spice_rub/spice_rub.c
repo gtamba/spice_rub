7fd854068d9f822207a5b15a626abb05d7661a91
Gaurav Tamba <gaurav.tamba@gmail.com>
2016-06-07 09:19:55 +0530

    added subpnt

--- ext/spice_rub/spice_rub.c
+++ ext/spice_rub/spice_rub.c
@@ -134,11 +134,13 @@ static VALUE kclear(VALUE self) {
 static VALUE latrec(VALUE self, VALUE radius, VALUE longitude, VALUE latitude, VALUE result) {
 
   latrec_c(NUM2DBL(radius), NUM2DBL(longitude), NUM2DBL(latitude),  NM_STORAGE_DENSE(result)->elements);
-  return Qtrue; 
+  return result; 
 }
 
 static VALUE reclat(VALUE self, VALUE rectangular_point) {
-  double radius, longitude, latitude;
+  double radius, 
+         longitude, 
+         latitude;
 
   reclat_c(NM_STORAGE_DENSE(rectangular_point)->elements, &radius, &longitude, &latitude);
   
@@ -150,10 +152,8 @@ static VALUE reclat(VALUE self, VALUE rectangular_point) {
 static VALUE lspcn(int argc, VALUE *argv, VALUE self) {
   double result;
 
-  //Default aberration correction is "NONE"
-  if(argc == 3) result = lspcn_c(StringValuePtr(argv[0]), NUM2DBL(argv[1]), StringValuePtr(argv[2]));
-  else result = lspcn_c(StringValuePtr(argv[0]), NUM2DBL(argv[1]), "NONE");   
-  
+  result = lspcn_c(StringValuePtr(argv[0]), NUM2DBL(argv[1]), StringValuePtr(argv[2]));
+
   if(spice_error(SPICE_ERROR_SHORT)) return Qnil;
 
   return DBL2NUM(result); 
@@ -202,9 +202,9 @@ static VALUE sincpt(int argc, VALUE *argv, VALUE self) {
  
   //Output parameters
   SpiceBoolean found;
-  double intercept_epoch;
-  double surface_point[3];
-  double surface_vector[3];
+  double intercept_epoch,
+         surface_point[3],
+         surface_vector[3];
 
   //Arrays that we return to Ruby
   VALUE rb_vector = rb_ary_new();
@@ -213,24 +213,13 @@ static VALUE sincpt(int argc, VALUE *argv, VALUE self) {
   //Loop control
   int count;
 
-  if (found == 0) {
-    rb_raise(rb_eRuntimeError, "Intercept point not found");
-    return Qnil;
-  }
-
   for(count = 0; count < 3; count++) {
-  	ray_direction_vector[count] = NUM2DBL(RARRAY_PTR(argv[5])[count]);
+  	//Should be nmatrix?
+    ray_direction_vector[count] = NUM2DBL(RARRAY_PTR(argv[6])[count]);
   }
   
-  if(argc == 7) {
-    sincpt_c(method, StringValuePtr(argv[0]), NUM2DBL(argv[1]), StringValuePtr(argv[2]), StringValuePtr(argv[3]), StringValuePtr(argv[4]), StringValuePtr(argv[5]), ray_direction_vector, surface_point, &intercept_epoch, surface_vector, &found);
-  }
+  sincpt_c(method, StringValuePtr(argv[0]), NUM2DBL(argv[1]), StringValuePtr(argv[2]), StringValuePtr(argv[3]), StringValuePtr(argv[4]), StringValuePtr(argv[5]), ray_direction_vector, surface_point, &intercept_epoch, surface_vector, &found);
 
-  else {
-    sincpt_c(method, StringValuePtr(argv[0]), NUM2DBL(argv[1]), StringValuePtr(argv[2]), "NONE", StringValuePtr(argv[3]), StringValuePtr(argv[4]), ray_direction_vector, surface_point, &intercept_epoch, surface_vector, &found);
-  }
-
-  }
   if(!found) {
   	return Qfalse;
   }
@@ -240,12 +229,64 @@ static VALUE sincpt(int argc, VALUE *argv, VALUE self) {
   }
 
   for (count = 0; count < 3; count++) {
-    rb_ary_push(rb_vector, rb_float_new(surface_vector[count]));
-    rb_ary_push(rb_point, rb_float_new(surface_point[count]));
+    rb_ary_push(rb_vector, DBL2NUM(surface_vector[count]));
+    rb_ary_push(rb_point, DBL2NUM(surface_point[count]));
   }  
 
   return rb_ary_new3(3, rb_point, rb_vector, DBL2NUM(intercept_epoch));
 }
+
+/*
+
+   void subpnt_c ( ConstSpiceChar       * method,
+                   ConstSpiceChar       * target,
+                   SpiceDouble            et,
+                   ConstSpiceChar       * fixref,
+                   ConstSpiceChar       * abcorr,
+                   ConstSpiceChar       * obsrvr,
+                   SpiceDouble            spoint [3],
+                   SpiceDouble          * trgepc,
+                   SpiceDouble            srfvec [3]
+
+*/
+
+static VALUE subpnt(int argc, VALUE *argv, VALUE self) {
+  //C containers to pass on to SPICE function
+  
+  /* Input
+   argv[0] -> Method
+   argv[1] -> Target
+   argv[2] -> Ephemeris Time
+   argv[3] -> Fixed Reference Object   
+   argv[4] -> Abb. Correction
+   argv[5] -> Observer
+  */
+
+  //loop control
+  int count;
+
+  //Output
+  double observer_epoch,
+         surface_point[3],
+         surface_vector[3];
+
+  //Arrays that we return to Ruby
+  VALUE rb_vector = rb_ary_new();
+  VALUE rb_point  = rb_ary_new();
+
+  subpnt_c(StringValuePtr(argv[0]), StringValuePtr(argv[1]), NUM2DBL(argv[2]), StringValuePtr(argv[3]), StringValuePtr(argv[4]), StringValuePtr(argv[5]), surface_point, &observer_epoch, surface_vector);
+
+  if(spice_error(SPICE_ERROR_SHORT)) return Qnil;
+
+  for (count = 0; count < 3; count++) {
+    rb_ary_push(rb_vector, DBL2NUM(surface_vector[count]));
+    rb_ary_push(rb_point, DBL2NUM(surface_point[count]));
+  }  
+
+  return rb_ary_new3(3, rb_point, rb_vector, DBL2NUM(observer_epoch));
+}
+
+
 //End of Geometry and Co-Ordinate Functions
 
 //Time and Time Conversions Functions
@@ -275,7 +316,9 @@ void Init_spice_rub(){
   rb_define_module_function(spicerub_module, "reclat", reclat, 1);
   rb_define_module_function(spicerub_module, "lspcn", lspcn, -1);
   rb_define_module_function(spicerub_module, "sincpt", sincpt, -1);
-  //rb_define_module_function(spicerub_module, "subpnt", subpnt, -1);
+  rb_define_module_function(spicerub_module, "subpnt", subpnt, -1);
+  
+
   //Atttach Time and Time Conversion functions
   rb_define_module_function(spicerub_module, "str2et", str2et, 1);
   

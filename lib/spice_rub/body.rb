ed14570cec68f4318b0c816755408c833c5b1376
Gaurav Tamba <gaurav.tamba@gmail.com>
2016-08-16 21:40:17 +0530

    Complete ::Body and ::Time

--- lib/spice_rub/body.rb
+++ lib/spice_rub/body.rb
@@ -19,43 +19,132 @@ module SpiceRub
     attr_reader :code , :name, :type
 
     def initialize(name , frame: :J2000)
-
-      if name.is_a? Integer
-        @name = SpiceRub::Native.bodc2n(name).downcase
+      case name
+      when Integer
+        @name = Native.bodc2n(name).downcase
         @code = name
-      elsif name.is_a? Symbol
+      when Symbol
         @name = name.downcase
-        @code = SpiceRub::Native.bodn2c(name)
+        @code = Native.bodn2c(name)
       else
         raise(ArgumentError, "argument must be a NAIF Integer ID or a key symbol")
       end  	
       
       @type = body_type(@code)
       @frame = frame
-      #raise(SpiceError, "body not found in SPICE Data") if !(@code and @name)
+      raise(SpiceError, "body not found in SPICE Data") if ( !@code and !@name )
     end
     
     def position_at(time, observer: :sun, frame: @frame, aberration_correction: nil, with_light_time: nil)
       #
-      observer = observer.name if observer.is_a? SpiceRub::Body
+      observer = observer.name if observer.is_a? Body
       aberration_correction = :none unless aberration_correction
-
-      case
-      when time.is_a?(SpiceRub::Time)
-        output = SpiceRub::Native.spkpos(self.name, time.to_et, frame, aberration_correction, observer)
-        with_light_time ? output : output[0]
-      when time.is_a?(Integer) 
-        5
-      end              
       
+      case time
+      when Time 
+        output = Native.spkpos(self.name, time.et, frame, aberration_correction, observer)
+        with_light_time ? output : output[0]
+      when Array 
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") unless time.all? { |t| t.is_a? Time } 
+        
+        output = []
+
+        time.each do |epoch| 
+          position = Native.spkpos(self.name, epoch.et, frame, aberration_correction, observer)
+          output << ( with_light_time ? position : position[0] ) 
+        end
+        output
+      else
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") 
+      end      
+    end
+    
+    def state_at(time, observer: :sun, frame: @frame, aberration_correction: nil, with_light_time: nil)
+      #
+      observer = observer.name if observer.is_a? Body
+      aberration_correction = :none unless aberration_correction
+      
+      case time
+      when Time
+        output = Native.spkezr(self.name, time.et, frame, aberration_correction, observer)
+        with_light_time ? output : output[0]
+      when Array
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") unless time.all? { |t| t.is_a? Time } 
+        
+        output = []
+
+        time.each do |epoch|
+          state = Native.spkezr(self.name, epoch.et, frame, aberration_correction, observer)
+          output << ( with_light_time ? state : state[0] )
+        end
+        output
+      else
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") 
+      end      
+    end
+    
+
+    def velocity_at(time, observer: :sun, frame: @frame, aberration_correction: nil, with_light_time: nil)
+      #
+      observer = observer.name if observer.is_a? Body
+      aberration_correction = :none unless aberration_correction
+      
+      case time
+      when Time
+        output = Native.spkezr(self.name, time.et, frame, aberration_correction, observer)
+        with_light_time ?  [output[0][0..2], output[1]] : output[0][0..2]
+      when Array
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") unless time.all? { |t| t.is_a? Time } 
+        
+        output = []
+
+        time.each do |epoch|
+          state = Native.spkezr(self.name, epoch.et, frame, aberration_correction, observer)
+          output << ( with_light_time ? [state[0][3..5], state[1]] : state[0][3..5] )
+        end
+        output
+      else
+        raise(ArgumentError, "Expected instance(s) of SpiceRub::Time") 
+      end      
     end
 
+    def light_time_from(target, time, frame: @frame, aberration_correction: nil)
+      raise(ArgumentError, "Expected instance of SpiceRub::Time") unless time.is_a? Time 
+
+      target = target.name if target.is_a? Body
+      aberration_correction = :none unless aberration_correction
+      
+      Native.spkpos(target, time.et, frame, aberration_correction, self.name)[1]
+    end
+
+    def distance_from(target, time, frame: @frame, aberration_correction: nil)
+      raise(ArgumentError, "Expected instance of SpiceRub::Time") unless time.is_a? Time 
+
+      target = target.name if target.is_a? Body
+      aberration_correction = :none unless aberration_correction
+      
+      position = Native.spkpos(target, time.et, frame, aberration_correction, self.name)[0]
+      Math.sqrt( (position ** 2).sum[0] )      
+    end
+
+    def within_proximity(body, distance, time, frame: @frame, aberration_correction: nil)
+      case body
+      when Body
+        body.distance_from(self) < distance ? [body] : nil 
+      when Array
+        ##todo
+     else
+     end
+    end
+    
     def body_type(body_id)
       case
         when body_id == 10
           return :star
         when /[1-9]99/ === body_id.to_s
           return :planet
+        when /[1-9]/ === body_id.to_s
+          return :barycenter
         #
         # etc.
         else 
--- lib/spice_rub/time.rb
+++ lib/spice_rub/time.rb
@@ -1,3 +1,20 @@
+#--
+# = SpiceRub
+#
+# A wrapper to the SPICE TOOLKIT for space and astronomomical
+# computation in Ruby.
+#
+#
+# == time.rb
+#
+# Contains the Time class, which is a wrapper class which always references
+# Ephemeris Time (TDB seconds past J2000 Epoch) but has numerous class methods
+# to construct from different time systems.
+# 
+# Requires a loaded leap second kernel.
+#
+#++
+
 module SpiceRub
   class Time
     
@@ -7,49 +24,79 @@ module SpiceRub
     
     @@system = :utc
 
-    def initialize(*params)
-      if params.empty?
-        @et = Native.str2et(Object::Time.now.utc.to_s)      
-      elsif params.size == 2 and params[1] == :et
-        @et = params[0]
-      else      
-        year  = params[0]      
-        month = params[1] || 1
-        day   = params[2] || 1
-        hour  = params[3] || 0
-        min   = params[4] || 0
-        sec   = params[5] || 0 
-        
-        @et = Native.str2et("#{month}/#{day}/#{year} #{hour}:#{min}#{sec}")
+    def initialize(epoch, seconds: :tdb)
+      case seconds
+      when :tdb
+        @et = epoch
+      when :utc
+        deltet = Native.deltet(epoch, :utc)
+        #ET = UTC + DELTET
+        @et = epoch + deltet   
+      when :tai, :tdt, :jdtdt, :tdb, :jed, :jdtdb
+        @et = Native.unitim(epoch, seconds, :et)
       end
     end
     
-    def self.at(offset, scale = :j2000)  
-      case scales.downcase
+    def self.at(offset, reference = :j2000, seconds: :tdb)  
+      raise(ArgumentError, "Non-TDB seconds must reference J2000 Epoch") if 
+        (![:utc, :tdb].include?(seconds) and 
+      	  reference.downcase != :j2000)
+
+      case reference.downcase
       when :j2100
-        new(offset + 3155760000.0, :et)
+        new(offset + 3155760000.0, seconds: seconds)
       when :j2000, :et
-        new(offset, :et)      
+        new(offset, seconds: seconds)      
       when :j1950
-        new(offset - 1577880000.0, :et)
+        new(offset - 1577880000.0, seconds: seconds)
       when :j1900
-        new(offset - 3155760000.0, :et)
-      when :tai, :tdt, :jdtdt, :tdb, :jed, :jdtdb
-        et = Native.unitim(offset, scale, :et)
-        new(et, :et)
+        new(offset - 3155760000.0, seconds: seconds)
+      when :gps
+        new(offset - 630763148.8159368, seconds: seconds)
       when :unix
-        et = Native.str2et(Object::Time.at(offset).utc.to_s)
-        new(et, :et)
+        new(offset - 946727958.8160644, seconds: seconds)
       end
     end
+    
+    def self.from_spacecraft_clock(spacecraft_id, time, encoding: :string)
+      case encoding
+      when :string
+        raise(ArgumentError, "expected spacecraft clock string") unless time.is_a? String
+        raise(ArgumentError, "expected integer spacecraft ID") unless spacecraft_id.is_a? Integer   
+        new(Native.scs2e(spacecraft_id, time))
+      when :ticks
+        raise(ArgumentError, "expected float encoded spacecraft time (ticks)") unless time.is_a? Float
+        raise(ArgumentError, "expected integer spacecraft ID") unless spacecraft_id.is_a? Integer   
+        new(Native.sct2e(spacecraft_id, time))
+      else
+        raise(ArgumentError, "encoding must be :string or :ticks")
+      end 
+    end
+    #alias :from_sc :from_spacecraft_clock
 
     def self.parse(string)
-      new(Native.str2et(string), :et)    
+      new(Native.str2et(string))    
     end
 
     def self.from_time(time)
       warn "Default time system is not UTC, Ruby's Time class only supports UTC and its offsets" unless @@system == :utc
-      new(Native.str2et(time.utc.to_s), :et)
+      new(Native.str2et(time.utc.to_s))
+    end
+    
+    def self.from_tuple(*params)
+      order = [:year, :month, :day, :hour, :min, :sec]
+	    components = [nil, 1, 1, 0, 0, 0]
+	
+	    components = order.zip(components, params)
+        .map { |sym, default, val| [sym, val || default] }.to_h
+	    
+      new(Native.str2et('%<month>02i/%<day>02i/%<year>04i %<hour>02i:%<min>02i:%<sec>02i' % 
+        components))    
+    end
+
+    def self.now
+      et = Native.str2et(::Time.now.utc.to_s)
+      new(et)
     end
     
     def to_tai
@@ -76,8 +123,29 @@ module SpiceRub
       Native.unitim(@et, :et, :jdtdb)
     end
 
-    def utc_to_tdb(epoch , reference)
-      #todo
+    def to_utc
+      @et - Native.deltet(@et, :et)
     end
+
+    def to_string(format = "Wkd Mon DD HR:MN:SC UTC YYYY ::UTC")
+      timout(@et, format, 32)
+    end
+    alias :to_s :to_string
+
+
+    def to_spacecraft_clock(spacecraft_id, encoding: :string)
+      raise(ArgumentError, "expected integer spacecraft ID") unless spacecraft_id.is_a? Integer   
+      ticks = sce2c(spacecraft_id, @et)  
+      
+      case encoding
+      when :ticks
+        ticks
+      when :string
+        scdecd(spacecraft_id, ticks, 32)
+      else
+        raise(ArgumentError, "encoding must be :string or :ticks")
+      end
+    end
+    alias :to_sc :to_spacecraft_clock
   end
 end
